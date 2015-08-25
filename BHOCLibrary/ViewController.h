d8551641b8cb8ecc185a0d414ace30a9fb660ce6
boyce <hepeilin1987@qq.com>
2015-08-25 17:05:14 +0800

    init

---
+++ BHOCLibrary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BHOCLibrary
+//
+//  Created by heboyce on 8/25/15.
+//  Copyright © 2015 boycehe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BHOCLibrary/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BHOCLibrary
+//
+//  Created by heboyce on 8/25/15.
+//  Copyright © 2015 boycehe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
